19110913c53ec492175de6cae1e07edc61ae720a
Marco Laspe <marco@rockiger.com>
2017-06-15 10:47:12 +0200

    create PixelGraphicFilter class without abstract class

---
+++ src/myimg/PixelGraphicFilter.java
@@ -0,0 +1,19 @@
+package myimg;
+
+import java.awt.image.BufferedImage;
+
+public class PixelGraphicFilter implements Filter {
+
+	@Override
+	public BufferedImage process(BufferedImage... bufferedImages) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+//  calculate(pixel : int [], maskPixel [] : int[], index : int, width : int, height : int) : int
+	
+	protected int calculate(int[] pixel, int [] maskPixel, int index, int width, int height) {
+		// TODO
+		return 0;
+	}
+	
+}
